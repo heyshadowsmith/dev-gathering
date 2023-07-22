23906a257b48dcb7185c9fb77f6b4a78e919ac70
Shadow Smith <hey@shadowsmith.com>
2023-07-22 00:25:17 -0500

    Add updates

--- app.config.ts
+++ app.config.ts
@@ -1,26 +1,8 @@
 export default defineAppConfig({
   docus: {
     title: 'Dev Gathering',
-    description: 'An Upper Middle Tennessee Software Development Meetup',
+    description: 'An Upper Middle Tennessee Software Development Community',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
-
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'dev-gathering',
-      owner: 'heyshadowsmith',
-      edit: true
-    },
 
     aside: {
       level: 0,
@@ -48,6 +30,6 @@ export default defineAppConfig({
       }
     },
 
-    titleTemplate: '%s · Dev Gathering'
+    titleTemplate: '%s | Dev Gathering'
   }
 })
---
+++ components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <img v-if="colorMode.preference === 'light'" src="/img/dev-gathering-logo-light.svg">
+  <img v-else src="/img/dev-gathering-logo-dark.svg">
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+</script>
