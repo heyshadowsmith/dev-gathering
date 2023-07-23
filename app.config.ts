a66a14e92526aef4ae5a85469cdc8d9f098aa1d9
Shadow Smith <hey@shadowsmith.com>
2023-07-22 21:29:59 -0500

    Updates

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,14 @@ export default defineAppConfig({
     description: 'An Upper Middle Tennessee Software Development Community',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
 
+    socials: {
+      discord: {
+        label: 'Discord',
+        icon: 'cib:discord',
+        href: 'https://discord.gg/dsy7e5RyPe'
+      }
+    },
+
     aside: {
       level: 0,
       collapsed: false,
